d39927ff9d534a80c1ff483ce294d3cf88c891fc
Felipe Marinho <github@felipemarinho.dev>
2025-12-16 21:29:51 -0800

    feat: create User table and update schema with new fields

---
+++ backend/prisma/prisma.ts
@@ -0,0 +1,7 @@
+import { PrismaClient } from "../src/generated/prisma/client";
+
+const globalForPrisma = global as unknown as { prisma: PrismaClient };
+
+export const prismaClient = globalForPrisma.prisma || new PrismaClient();
+
+globalForPrisma.prisma = prismaClient;
