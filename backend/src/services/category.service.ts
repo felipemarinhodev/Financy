1c19ff68e3c643ad8b486a77c7c552948db91fbc
Felipe Marinho <github@felipemarinho.dev>
2025-12-17 07:52:21 -0800

    feat: implement CategoryService with create method for category creation

---
+++ backend/src/services/category.service.ts
@@ -0,0 +1,30 @@
+import { prismaClient } from "@/prisma/prisma";
+import { CreateCategoryInput } from "../dtos/input";
+
+export class CategoryService {
+  async create(
+    { title, color, description, icon }: CreateCategoryInput,
+    userId: string
+  ) {
+    const category = await prismaClient.category.create({
+      data: {
+        title,
+        color,
+        description,
+        icon,
+        userId,
+      },
+    });
+
+    return {
+      id: category.id,
+      title: category.title,
+      description: category.description,
+      color: category.color,
+      icon: category.icon,
+      userId: category.userId,
+      createdAt: category.createdAt,
+      updatedAt: category.updatedAt,
+    };
+  }
+}
