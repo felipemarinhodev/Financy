b5fabd7156e18942a11cdf9b961afad61a85689c
Felipe Marinho <github@felipemarinho.dev>
2025-12-16 21:52:10 -0800

    feat: add password comparison utility to hash module

--- backend/src/utils/hash.ts
+++ backend/src/utils/hash.ts
@@ -1,6 +1,13 @@
-import bycrypjs from "bcryptjs";
+import bycrypjs, { compare } from "bcryptjs";
 
 export const hashPassword = async (plainPassword: string) => {
   const salt = await bycrypjs.genSalt(10);
   return bycrypjs.hash(plainPassword, salt);
-}
+};
+
+export const comparePassword = async (
+  plainPassword: string,
+  hashedPassword: string
+) => {
+  return await compare(plainPassword, hashedPassword);
+};
