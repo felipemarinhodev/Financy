9584615e19b42d786056283cc93036680520dbfe
Felipe Marinho <github@felipemarinho.dev>
2025-12-17 07:50:31 -0800

    feat: create CreateCategoryInput DTO for category creation

---
+++ backend/src/dtos/input/category.input.ts
@@ -0,0 +1,15 @@
+import { ColorCategory, IconCategory } from "@/src/models";
+import { Field, InputType } from "type-graphql";
+
+@InputType()
+export class CreateCategoryInput {
+
+  @Field(() => String)
+  title!: string;
+  @Field(() => String, { nullable: true })
+  description?: string;
+  @Field(() => ColorCategory, { nullable: true })
+  color?: ColorCategory;
+  @Field(() => IconCategory, { nullable: true })
+  icon?: IconCategory;
+}
