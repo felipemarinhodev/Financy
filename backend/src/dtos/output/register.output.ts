708ec0cb271acd9f4f3151442b89e894e57ee9f4
Felipe Marinho <github@felipemarinho.dev>
2025-12-16 21:31:06 -0800

    feat: create RegisterOutput DTO for user registration response

---
+++ backend/src/dtos/output/register.output.ts
@@ -0,0 +1,12 @@
+import { UserModel } from "@/src/models/user.model";
+import { Field, ObjectType } from "type-graphql";
+
+@ObjectType()
+export class RegisterOutput {
+  @Field(() => String)
+  token!: string;
+  @Field(() => String)
+  refreshToken!: string;
+  @Field(() => UserModel)
+  user!: UserModel;
+}
