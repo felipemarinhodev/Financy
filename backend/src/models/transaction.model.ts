77470f492490d813b9a51368a26a01694f040a6b
Felipe Marinho <github@felipemarinho.dev>
2025-12-17 21:31:48 -0800

    feat: add TransactionModel with fields and TransactionType enum

---
+++ backend/src/models/transaction.model.ts
@@ -0,0 +1,40 @@
+import {
+  Field,
+  GraphQLISODateTime,
+  ID,
+  ObjectType,
+  registerEnumType
+} from "type-graphql";
+import { CategoryModel, UserModel } from "../models";
+
+export enum TransactionType {
+  income = "income",
+  expense = "expense",
+}
+
+registerEnumType(TransactionType, {
+  name: "TransactionType",
+  description: "Types of transactions",
+});
+
+@ObjectType()
+export class TransactionModel {
+  @Field(() => ID)
+  id!: string;
+  @Field(() => String)
+  description!: string;
+  @Field(() => TransactionType)
+  type!: string;
+  @Field(() => Number)
+  amount!: number;
+  @Field(() => String)
+  categoryId!: string;
+  @Field(() => String)
+  userId!: string;
+  @Field(() => GraphQLISODateTime)
+  date!: Date;
+  @Field(() => UserModel, { nullable: true })
+  user?: UserModel;
+  @Field(() => CategoryModel, { nullable: true })
+  category?: CategoryModel;
+}
