4821c401426d601df2639769d47a3387330c3caf
Felipe Marinho <github@felipemarinho.dev>
2025-12-21 11:13:58 -0800

    feat: implement Signup component with form validation and account creation functionality

--- frontend/src/pages/Auth/Signup.tsx
+++ frontend/src/pages/Auth/Signup.tsx
@@ -1,3 +1,69 @@
+import { TextField } from "@/components/TextField";
+import { Button } from "@/components/ui/button";
+import { LogIn } from "lucide-react";
+import { useState } from "react";
+import { Link } from "react-router";
+import { useAuthController } from "./useAuthController";
+
 export function Signup() {
-  return <div>Signup Page</div>;
+  const { handleCreateAccount, isLoading } = useAuthController();
+
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  return (
+    <main className="flex flex-col gap-4 bg-gray-100 h-screen justify-center p-4">
+      <img src="/Logo.svg" alt="Financy" className="w-32 mx-auto" />
+      <div className="flex flex-col gap-4 justify-center items-center bg-white rounded-2xl p-8 max-w-110 mx-auto w-full">
+        <h2 className="text-xl font-bold">Criar conta</h2>
+        <p>Comece a controlar suas finanças ainda hoje</p>
+        <TextField
+          label="Nome completo"
+          icon="user"
+          type="text"
+          placeholder="Seu nome completo"
+          value={name}
+          onChange={(e) => setName(e.target.value)}
+          disabled={isLoading}
+        />
+        <TextField
+          label="E-mail"
+          icon="mailbox"
+          type="email"
+          placeholder="mail@exemplo.com"
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+          disabled={isLoading}
+        />
+        <TextField
+          label="Senha"
+          icon="lock"
+          type="password"
+          placeholder="Digite sua senha"
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+          disabled={isLoading}
+        />
+        <Button
+          className="w-full mt-4"
+          onClick={() => handleCreateAccount(name, email, password)}
+          disabled={isLoading}
+          size="lg">
+          Entrar
+        </Button>
+        <div className="flex flex-row items-center w-full">
+          <hr className="border-gray-300 w-full" />
+          <span className="text-md text-gray-600 mx-2">ou</span>
+          <hr className="border-gray-300 w-full" />
+        </div>
+        <span className="text-md text-gray-600">Já tem uma conta?</span>
+        <Link to="/" className="w-full">
+          <Button variant="outline" className="w-full" size="lg">
+            <LogIn className="w-4 h-4 mr-2" /> Fazer login
+          </Button>
+        </Link>
+      </div>
+    </main>
+  );
 }
--- frontend/src/pages/Auth/useAuthController.ts
+++ frontend/src/pages/Auth/useAuthController.ts
@@ -1,11 +1,13 @@
 import { useAuthStore } from "@/stores/auth";
+import type { RegisterInput } from "@/types";
 import { useCallback, useState } from "react";
 import { useNavigate } from "react-router";
 import { toast } from "sonner";
+import z from "zod";
 
 export const useAuthController = () => {
   const [isLoading, setIsLoading] = useState(false);
-  const login = useAuthStore((state) => state.login);
+  const { login, signup } = useAuthStore((state) => state);
   const navigate = useNavigate();
 
   const handleLogin = useCallback(
@@ -29,8 +31,40 @@ export const useAuthController = () => {
     [login, navigate]
   );
 
+  const userSchema = z.object({
+    name: z.string().min(3, "Name must be at least 3 characters long"),
+    email: z.email("Invalid email address"),
+    password: z.string().min(6, "Password must be at least 6 characters long"),
+  });
+
+  const handleCreateAccount = useCallback(
+    async (name: string, email: string, password: string) => {
+      try {
+        setIsLoading(true);
+        const newUser = userSchema.parse({
+          name,
+          email,
+          password,
+        }) as unknown as RegisterInput;
+        const isCreated = await signup(newUser);
+        if (isCreated) {
+          toast.success(`Account created successfully! Welcome, ${name}.`);
+          navigate("/dashboard");
+        }
+      } catch (error) {
+        setIsLoading(false);
+        toast.error("Login failed! Please check your credentials.");
+        throw error;
+      } finally {
+        setIsLoading(false);
+      }
+    },
+    [signup, navigate, userSchema]
+  );
+
   return {
     handleLogin,
+    handleCreateAccount,
     isLoading,
   };
 };
